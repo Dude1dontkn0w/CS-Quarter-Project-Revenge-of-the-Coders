e3051403c5016ea635ce7c70ed9f9fae7efda6bd
Dude1dontkn0w <117276286+Dude1dontkn0w@users.noreply.github.com>
2023-03-20 23:06:34 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+  function clueget1(event){
+    //*function that will give you an item at main game that is activated at minigame 1
+  }
+    function clueget2electricbogaloo(event){
+    //*function that will give you an item at main game that is activated at minigame 2
+  }
+</script>
